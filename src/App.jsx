2fb393299dbba8010c322eff73022d6e3a84163e
Allandev <allanjeje@gmail.com>
2024-08-13 20:00:42 -0300

    React router
    Estudos sobre React router , afim de entender a funcionalidade das propriedades Router, switch que agora está em desuso e foi substituída pela propriedade Routes. Também como é a funcionalidade nova do route e também do LINK

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Empresa from './components/pages/Empresa';
+import Contato from './components/pages/Contato';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
+
 
 
 function App() {
 
-  return(
-
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/Empresa" element={<Empresa />}/>
+        <Route path="/Contato" element={<Contato />}/>
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
